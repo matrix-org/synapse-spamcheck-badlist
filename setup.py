f8f3b69a60f7be333c3fa0f9840ce373f1b3599a
David Teller <d.o.teller+github@gmail.com>
2021-01-06 18:19:37 +0100

    Telemetry: Measuring speed of link-checking.
    Looking at the source code of `linkify`, I have the strong suspicion that it will be very slow to execute. Let's make sure that we get this information as early as possible.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="A Synapse spam filter designed to block links and upload of content already known as bad. The typical use case is to plug this with a list of links and MD5s of child sexual abuse, as published by the IWF.",
     include_package_data=True,
     zip_safe=True,
-    install_requires=['linkify-it-py'],
+    install_requires=['linkify-it-py', 'prometheus-client'],
     author="David Teller",
     author_email="davidt@element.io",
     license="Apache 2",
