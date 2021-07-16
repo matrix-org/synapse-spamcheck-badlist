e9eb830375ac1629b22914189d440072de83cbea
David Teller <d.o.teller+github@gmail.com>
2021-07-16 11:24:57 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="synapse-spamcheck-badlist",
-    version="0.2.3",
+    version="0.2.4",
     packages=find_packages(),
     description="A Synapse spam filter designed to block links and upload of content already known as bad. The typical use case is to plug this with a list of links and MD5s of child sexual abuse, as published by the IWF.",
     include_package_data=True,
